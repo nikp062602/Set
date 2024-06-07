6eee1123c4f45e052313da381103a0b7e0bb47f0
nikp062602 <nikp062602@gmail.com>
2024-06-07 04:34:46 -0500

    first

--- src/main/java/com/example/set_exercise/HelloApplication.java
+++ src/main/java/com/example/set_exercise/HelloApplication.java
@@ -7,39 +7,33 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
+import java.util.Scanner;
+
 public class HelloApplication {
     public static void main(String[] args) {
-        String[] firstarray = new String[]{"2", "4", "10", "11", "12"};
-        String[] secondarray = new String[]{"30", "2", "4", "55", "80","Hola","Chao"};
-        String[] thirdarray = new String[]{"2", "33", "55", "Hola"};
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Introduce el número de escalones: ");
+        int escalones = scanner.nextInt();
 
-        findUniqueElements(firstarray, secondarray, thirdarray);
-    }
+        if (escalones == 0) {
+            System.out.println("__");
+        } else if (escalones>0) {
+            for (int i = 0; i<escalones; i++) {
+                for (int j = 0; j<escalones-i-1; j++) {
+                    System.out.print("  ");
+                }
+                System.out.println("_|");
+            }
+        } else {
+            escalones = -escalones;
+            for (int i = 0; i < escalones; i++) {
+                for (int j = 0; j < i; j++) {
+                    System.out.print("  ");
+                }
+                System.out.println("|_");
+            }
+        }
 
-    public static void findUniqueElements(String[] firstarray, String[] secondarray, String[] thirdarray) {
-        for (String element : firstarray) {
-            if (!containsElement(element, secondarray) && !containsElement(element, thirdarray)) {
-                System.out.println("Elemento único encontrado: " + element);
-            }
-        }
-        for (String element : secondarray) {
-            if (!containsElement(element, firstarray) && !containsElement(element, thirdarray)) {
-                System.out.println("Elemento único encontrado: " + element);
-            }
-        }
-        for (String element : thirdarray) {
-            if (!containsElement(element, firstarray) && !containsElement(element, secondarray)) {
-                System.out.println("Elemento único encontrado: " + element);
-            }
-        }
-    }
-
-    public static boolean containsElement(String element, String[] array) {
-        for (String item : array) {
-            if (item.equals(element)) {
-                return true;
-            }
-        }
-        return false;
+        scanner.close();
     }
 }
